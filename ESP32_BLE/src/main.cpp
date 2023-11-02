21d1fdffb22c3231838f543f6b6c74cb1320730e
Kou Fukuta <kou2022ms@nzu.jp>
2023-11-02 13:26:27 +0900

    Wi-Fiを使ってのRSSIを測定、表示
    Wi-Fiを使ってのRSSI検出に成功
    
    参考元
    https://fabcross.jp/category/make/sorem5/20210512_social_distance_onigokko.html

--- ESP32_BLE/src/main.cpp
+++ ESP32_BLE/src/main.cpp
@@ -1,5 +1,63 @@
 #include <Esp.h>
-#include "BluetoothSerial.h"
+
+#include <NimBLEDevice.h>
+#include <Ethernet.h>
+#include <ArduinoOSCWiFi.h>
+
+#define LED_BUILTIN 2
+
+// Wi-Fiの設定です passだけ設定してください
+const char *ssid = "M5_Send";
+const char *pwd = "M5rssiii"; //送信側で設定したものと同じものを使用
+const IPAddress ip(192, 168, 1, 201);
+const IPAddress gateway(192, 168, 1, 1);
+const IPAddress subnet(255, 255, 255, 0);
+
+// for ArduinoOSC
+const char *host = "192.168.1.255"; //念の為同じネットワーク内の全員に送信します
+const int incomingPort = 8080; // 受信ポート番号
+const int outgoingPort = 7070; // 送信ポート番号
+
+void setup(){
+  Serial.begin(115200);
+
+#ifdef ESP_PLATFORM
+  WiFi.disconnect(true, true);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+#endif
+  WiFi.begin(ssid, pwd);
+  WiFi.config(ip, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+}
+
+void loop()
+{
+  
+  int rssi = 0;
+  for (int i = 0; i < 100; i++)
+  {
+    rssi += WiFi.RSSI();
+  }
+  rssi = rssi / -100;
+  Serial.printf("rssi: %d", rssi);
+  OscWiFi.send(host, outgoingPort, "/rssi", rssi); //何もない時はRSSI値を送り続けます
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  OscWiFi.post();
+  
+  
+  delay(200);
+
+}
+
+
+/* 
+前のやつ
 
 BluetoothSerial SerialBT;
 
@@ -24,7 +82,7 @@ void loop() {
   delay(20);
   digitalWrite(14, LOW);
 }
-
+ */
 
 /*
 MACアドレス調べたいときのやつ 
--- M5Stack_sendBLE/src/main.cpp
+++ M5Stack_sendBLE/src/main.cpp
@@ -1,3 +1,75 @@
+#include <Arduino.h>
+#include <M5Core2.h>
+#include <AXP192.h>//バイブレーション使ってみる
+#include <Ethernet.h>
+#include <ArduinoOSCWiFi.h>
+
+AXP192 power;
+
+
+bool ONI = true;
+const int THRESH = -20; //タッチ判定をするRSSI値の閾値です。
+
+//Wi-Fi用の設定を以下に書きます　passだけ設定してください
+const char *ssid = "M5_Send";         // SSID
+const char *pass = "M5rssiii"; // passwordは自由に設定してください
+const int incomingPort = 7070;       // 受信ポート番号
+const int outgoingPort = 8080;       // 送信ポート番号
+
+const IPAddress ipServer(192, 168, 1, 1);   // server IPアドレス
+const IPAddress ipGateway(192, 168, 1, 1);  // gateway IPアドレス
+const IPAddress subnet(255, 255, 255, 0);   // サブネットマスク
+const IPAddress ipClient(192, 168, 1, 255); // client IPアドレス
+
+const char *host = "192.168.1.255";
+
+
+void rcv_rssi(const OscMessage &msg) //RSSIを受信した時に起動する関数です。
+{
+  int rssi = msg.arg<int>(0);
+
+  M5.Lcd.setCursor(0, 195);
+  M5.Lcd.printf("rssi: %d", rssi); //受信したRSSI値を画面に表示します
+
+
+}
+
+void setup()
+{
+  M5.begin();
+  M5.Lcd.clearDisplay();
+  M5.Lcd.setTextColor(GREEN, BLACK);
+  M5.Lcd.setTextSize(2);
+
+  WiFi.softAP(ssid, pass);
+  delay(100);
+  WiFi.softAPConfig(ipServer, ipGateway, subnet);
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.printf("ssid: %s", ssid);
+  M5.Lcd.setCursor(0, 65);
+  M5.Lcd.printf("incomingPort: %d", incomingPort);
+  M5.Lcd.setCursor(0, 105);
+  M5.Lcd.printf("outgoingPort: %d", outgoingPort);
+  delay(1000);
+
+  OscWiFi.subscribe(incomingPort, "/rssi", rcv_rssi); //この記述で、runからRSSI値が送られてきた時にrcv_rssi関数が起動するようになります。
+}
+
+void loop()
+{
+  OscWiFi.parse(); //何もない場合はrunからの通信を待ち続けます。
+}
+
+
+
+
+
+
+
+
+
+
+
 /*
 参考サイト
 https://www.remma.net/?p=367
@@ -6,9 +78,11 @@ ESP32 Bluetooth　
 MACアドレス : 9C:9C:1F:D0:34:7A
 */
 
-#include <Arduino.h>
+/* #include <Arduino.h>
 #include <M5Core2.h>
 #include "BluetoothSerial.h"
+
+#include <NimBLEDevice.h>
 
 BluetoothSerial SerialBT;
 
@@ -17,6 +91,7 @@ uint8_t address[6]  = {0x9C, 0x9C, 0x1F, 0xD0, 0x34, 0x7A};
 bool connected;
 
 void setup() {
+  M5.begin();
   Serial.begin(115200);
   SerialBT.begin("M5Stack_sendTest", true); 
   Serial.println("The device started in master mode, make sure remote BT device is on!");
@@ -43,3 +118,4 @@ void setup() {
 void loop() {
     SerialBT.write('T');
 }
+ */
